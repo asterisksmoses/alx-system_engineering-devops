8fd8d5812a490ad1e29bf6a9835ed40382d9d245
asterisksmoses <asterisksmoses@gmail.com>
2024-06-28 14:31:43 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,17 @@
 # This file installs flask from pip3 using puppet
 
-package {'Flask':
+package { 'python 3.8':
+  ensure   => '3.8.10',
+  provider => 'pip3',
+}
+
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => package['Flask'],
 }
